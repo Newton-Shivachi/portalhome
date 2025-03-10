d5e1848f775c8c662d74d936c914a1bf76e49656
Newton-Shivachi <163133938+Newton-Shivachi@users.noreply.github.com>
2025-03-10 14:35:14 +0300

    Update models.py

--- houses/models.py
+++ houses/models.py
@@ -12,7 +12,7 @@ class House(models.Model):
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     contact = models.CharField(max_length=20)
     is_taken = models.BooleanField(default=False)
-    video = CloudinaryField('house_videos/', blank=True, null=True)
+    video = CloudinaryField('house_video', resource_type='video', blank=True, null=True)
     def __str__(self):
         return self.name
 
