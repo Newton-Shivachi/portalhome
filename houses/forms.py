88062c27e7bf40034f43607b49255e7d7e94016e
Newton-Shivachi <163133938+Newton-Shivachi@users.noreply.github.com>
2025-03-29 12:34:38 +0300

    Update forms.py

--- houses/forms.py
+++ houses/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from .models import House, HouseImage
 
+from django import forms
+from .models import House, HouseImage
+
 class HouseForm(forms.ModelForm):
     class Meta:
         model = House
         fields = ['name', 'description', 'location', 'latitude', 'longitude', 'contact', 'is_taken', 'video']
+
+class HouseImageForm(forms.ModelForm):
+    class Meta:
+        model = HouseImage
+        fields = ['image']
 
 class HouseImageForm(forms.ModelForm):
     class Meta:
