988962fa79361f0e363ab5d29082d4f1902098c3
Newton-Shivachi <163133938+Newton-Shivachi@users.noreply.github.com>
2025-03-29 12:35:56 +0300

    Update forms.py

--- houses/forms.py
+++ houses/forms.py
@@ -1,18 +1,18 @@
 from django import forms
 from .models import House, HouseImage
 
-from django import forms
-from .models import House, HouseImage
-
 class HouseForm(forms.ModelForm):
+    images = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), required=False)
+
     class Meta:
         model = House
-        fields = ['name', 'description', 'location', 'latitude', 'longitude', 'contact', 'is_taken', 'video']
+        fields = ['name', 'description', 'location', 'latitude', 'longitude', 'contact', 'rent', 'video']
 
-class HouseImageForm(forms.ModelForm):
-    class Meta:
-        model = HouseImage
-        fields = ['image']
+    def save(self, commit=True):
+        house = super().save(commit=False)
+        if commit:
+            house.save()
+        return house
 
 class HouseImageForm(forms.ModelForm):
     class Meta:
